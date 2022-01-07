2f614668d5c2cfbf9d1a0f60641b11f63673ac77
ivan lopez <navi97_red@hotmail.com>
2022-01-07 18:41:39 +0000

    [ADD] CRM Ad

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
         
@@ -15,4 +16,24 @@ class Course(models.Model):
                                             ('intermediate','Intermediate'),
                                             ('advanced', 'Advanced')],
                                 copy=False)
+        
         active =fields.Boolean(string='Active', default=True)
+         
+        base_price =fields.Float(string='Base Price', default=0.00)
+        
+        additional_fee = fields.Float(string='Additional fee', default=10.00)
+        
+        total_price = fields.Float(String='Total Price', readonly=True)
+        
+        @api.onchange('base_price','adittional_fee')
+        def _onchange_total_price(self):
+            if self.base_price < 0.00:
+                raise UserError('Base Price cannot be set as Negative.')
+            
+            self.total_price = self.base_price + self.additional_fee
+        
+        @api.constrains('adittional_fee')
+        def _check_additional_fee(self):
+            for record in self:
+                if record.additional_fee < 10.00:
+                    raise ValidateError('Additional Fees cannot be less than 10.00: %s' % record.adittional_fee) 
