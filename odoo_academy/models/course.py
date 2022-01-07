fd1827684421960a31d711d30f6fa0bf613aacdb
ivan lopez <navi97_red@hotmail.com>
2022-01-07 21:17:02 +0000

    [ADD] security

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -25,7 +25,7 @@ class Course(models.Model):
         
         total_price = fields.Float(string='Total Price', readonly=True)
         
-        @api.onchange('base_price','adittional_fee')
+        @api.onchange('base_price','additional_fee')
         def _onchange_total_price(self):
             if self.base_price < 0.00:
                 raise UserError('Base Price cannot be set as Negative.')
