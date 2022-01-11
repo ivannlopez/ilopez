05b68805aeb11b7d71b9399b1b2e321cd7d13477
ivan lopez <navi97_red@hotmail.com>
2022-01-11 02:36:08 +0000

    [ADD] advance fields

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -25,7 +25,11 @@ class Course(models.Model):
         
         total_price = fields.Float(string='Total Price', readonly=True)
         
-        @api.onchange('base_price','adittional_fee')
+        session_ids = fields.One2many(comodel_name='academy.session',
+                                     inverse_name='course_id',
+                                     string='Sessions')
+        
+        @api.onchange('base_price','additional_fee')
         def _onchange_total_price(self):
             if self.base_price < 0.00:
                 raise UserError('Base Price cannot be set as Negative.')
